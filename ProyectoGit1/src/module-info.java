59542ca242d381b5012e978e325d9e67331b4899
Gxbri <Gxbri@DESKTOP-4OL3VJO>
2024-03-12 10:49:36 +0100

    original

---
+++ ProyectoGit1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProyectoGit1 {
+}
---
+++ ProyectoGit1/src/paquete1/PrincipalGit.java
@@ -0,0 +1,9 @@
+package paquete1;
+
+public class PrincipalGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
